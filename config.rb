7ecd08e5f1266f07eb622f5df339e711cd3edbae
Morten Klim Sørensen <mail@kl1m.dk>
2013-10-30 10:19:34 +0100

    source added

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/Tema-XP/"
+http_images_path = "/Tema-XP/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/Tema-XP/fonts"
+css_dir = "public/Tema-XP/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
