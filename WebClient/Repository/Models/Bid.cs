90a57f6c1f7ea088831ba706bf34983f41f73ef6
Morten Klim Sorensen <mail@kl1m.dk>
2013-10-31 12:38:57 +0100

    Auction, bid etc. added to repo

---
+++ WebClient/Repository/Models/Bid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Models
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Auction Auction { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
