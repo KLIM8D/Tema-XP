6925b36ecebe4b4937aa6add2995fae0c9da1aff
iammadsnielsen <contact@madsnielsen.net>
2013-10-31 12:04:47 +0100

    Added BPDbContext...

---
+++ WebClient/Repository/Models/BPDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+using Repository.Models.Mapping;
+
+namespace Repository.Models
+{
+    public class BPDbContext : DbContext
+    {
+        static BPDbContext()
+        {
+            Database.SetInitializer<BPDbContext>(null);
+        }
+
+        public BPDbContext() : base("Name=BPDbContext")
+        {
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Artwork> Artworks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ArtistMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new ArtworkMap());
+        }
+    }
+}
