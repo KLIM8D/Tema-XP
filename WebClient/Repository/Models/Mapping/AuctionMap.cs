0a163209e237e9fa14f607dc8c87dda3534d4130
Chris <christind@hotmail.com>
2013-11-01 11:55:14 +0100

    added auctionMap

---
+++ WebClient/Repository/Models/Mapping/AuctionMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Models.Mapping
+{
+    public class AuctionMap : EntityTypeConfiguration<Auction>
+    {
+        public AuctionMap()
+        {
+            this.HasKey(t => t.Id);
+
+            this.ToTable("Auctions");
+            this.Property(t => t.Id).HasColumnName("Id");
+        }         
+    }
+}
